4e51fdc0b49d87a0c2c8a236adf274b62342b10a
hoc-anms <phamvanhoc96@gmail.com>
2018-08-21 01:30:52 +0700

    create a basic form submit

---
+++ welcome_get.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hoc_Anms
+ * Date: 8/20/2018
+ * Time: 10:55 AM
+ */
+echo "Welcome ".$_POST["name"]."<br>";
+echo "Your email is ".$_POST["email"]."<br>";
